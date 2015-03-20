a3050fa66d2e898244924847287978326d1144bf
Jared Jennings <jjennings@fastmail.fm>
2015-03-20 17:09:38 -0500

    apply Apache 2.0 license

--- cmits-example/modules-unclass/stig_misc/manifests/run_control_scripts.pp
+++ cmits-example/modules-unclass/stig_misc/manifests/run_control_scripts.pp
@@ -1,15 +1,18 @@
-# % --- BEGIN DISCLAIMER ---
-# % Those who use this do so at their own risk;
-# % AFSEO does not provide maintenance nor support.
-# % --- END DISCLAIMER ---
-# % --- BEGIN AFSEO_DATA_RIGHTS ---
-# % This is a work of the U.S. Government and is placed
-# % into the public domain in accordance with 17 USC Sec.
-# % 105. Those who redistribute or derive from this work
-# % are requested to include a reference to the original,
-# % at <https://github.com/afseo/cmits>, for example by
-# % including this notice in its entirety in derived works.
-# % --- END AFSEO_DATA_RIGHTS ---
+# % CMITS - Configuration Management for Information Technology Systems
+# % Based on <https://github.com/afseo/cmits>.
+# % Copyright 2015 Jared Jennings <mailto:jjennings@fastmail.fm>.
+# %
+# % Licensed under the Apache License, Version 2.0 (the "License");
+# % you may not use this file except in compliance with the License.
+# % You may obtain a copy of the License at
+# %
+# %    http://www.apache.org/licenses/LICENSE-2.0
+# %
+# % Unless required by applicable law or agreed to in writing, software
+# % distributed under the License is distributed on an "AS IS" BASIS,
+# % WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# % See the License for the specific language governing permissions and
+# % limitations under the License.
 # \subsection{Secure run control scripts}
 
 class stig_misc::run_control_scripts {
